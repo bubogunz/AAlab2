5ca26b4ba8348ccea3f588d7f0c1b594c10f9cb9
bubogunz <federico.brian@gmail.com>
2020-05-17 22:53:27 +0200

    Update Main.java

--- JavaLab2/src/lab2/main/Main.java
+++ JavaLab2/src/lab2/main/Main.java
@@ -1,20 +1,8 @@
 package lab2.main;
 
-import java.util.ArrayList;
 public class Main {
-	
-	private static ArrayList<String> test() {
-		ArrayList<String> ret = new ArrayList<String>();
-		while(true) {
-			Long start = System.nanoTime();
-			Long end = System.nanoTime();
-			while((end - start)/1000000000 < 1) 
-				end = System.nanoTime();	
-			System.out.println("Hello!");
-			ret.add("Hello!");
-		}
-	}
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
 	}
 }
